fab39c1a23f71d153f77278d49710d57c05cf037
Eric T. Dawson <eric.t.dawson@gmail.com>
2015-11-03 20:34:37 +0000

    Reorganized submodules and source code

---
+++ src/utility.hpp
@@ -0,0 +1,36 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <omp.h>
+#include <cstring>
+#include "vg.pb.h"
+#include "sha1/sha1.hpp"
+
+namespace vg {
+
+using namespace std;
+
+char reverse_complement(const char& c);
+string reverse_complement(const string& seq);
+int get_thread_count(void);
+
+// split a string on any character found in the string of delimiters (delims)
+std::vector<std::string>& split_delims(const std::string &s, const std::string& delims, std::vector<std::string> &elems);
+std::vector<std::string> split_delims(const std::string &s, const std::string& delims);
+
+const std::string sha1sum(const std::string& data);
+const std::string sha1head(const std::string& data, size_t head);
+
+
+bool allATGC(string& s);
+void mapping_cigar(const Mapping& mapping, vector<pair<int, char> >& cigar);
+string cigar_string(vector<pair<int, char> >& cigar);
+string mapping_string(const string& source, const Mapping& mapping);
+void divide_invariant_mapping(Mapping& orig, Mapping& left, Mapping& right, int offset, Node* nl, Node* nr);
+
+}
+
+#endif
