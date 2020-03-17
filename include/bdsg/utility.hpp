1477934312c2cb75a9e3e07f89df38003c5c78b4
Adam Novak <anovak@soe.ucsc.edu>
2020-03-17 12:08:08 -0700

    Use extern C linkage for OMP forward declaration

--- include/bdsg/utility.hpp
+++ include/bdsg/utility.hpp
@@ -9,7 +9,9 @@
 // Binder can't always find OpenMP's headers.
 // So we hackily declare the one OppenMP function we use, to avoid including omp.h.
 // TODO: Make get_thread_count not inline instead?
-int omp_get_num_threads(void);
+extern "C" {
+    int omp_get_num_threads(void);
+}
 
 namespace bdsg {
 
