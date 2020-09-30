44ee32d5b633581eaad92220336d079a1cb65a93
Adam Novak <anovak@soe.ucsc.edu>
2020-09-30 14:25:12 -0700

    Revert "Try a fancier cmake build"
    This reverts commit a79a4bd5d72ac16df0d173940693e03111c2bee0.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # setup.py: setup script to install `bdsg` Python module
-# Adapted from https://stackoverflow.com/a/48015772 and https://stackoverflow.com/a/51575996
+# Adapted from https://stackoverflow.com/a/48015772
 
 import os
 import pathlib
@@ -9,189 +9,60 @@ from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext as build_ext_orig
 
 
-from distutils.command.install_data import install_data
-from setuptools import find_packages, setup, Extension
-from setuptools.command.build_ext import build_ext
-from setuptools.command.install_lib import install_lib
-from setuptools.command.install_scripts import install_scripts
-import struct
-
-BITS = struct.calcsize("P") * 8
-PACKAGE_NAME = "bdsg"
-
 class CMakeExtension(Extension):
-    """
-    An extension to run the cmake build
 
-    This simply overrides the base extension class so that setuptools
-    doesn't try to build your sources for you
-    """
+    def __init__(self, name):
+        # don't invoke the original build_ext for this special extension
+        super().__init__(name, sources=[])
 
-    def __init__(self, name, sources=[]):
 
-        super().__init__(name = name, sources = sources)
-
-class InstallCMakeLibsData(install_data):
-    """
-    Just a wrapper to get the install data into the egg-info
-
-    Listing the installed files in the egg-info guarantees that
-    all of the package files will be uninstalled when the user
-    uninstalls your package through pip
-    """
+class build_ext(build_ext_orig):
 
     def run(self):
-        """
-        Outfiles are the libraries that were built using cmake
-        """
-
-        # There seems to be no other way to do this; I tried listing the
-        # libraries during the execution of the InstallCMakeLibs.run() but
-        # setuptools never tracked them, seems like setuptools wants to
-        # track the libraries through package data more than anything...
-        # help would be appriciated
-
-        self.outfiles = self.distribution.data_files
-
-class InstallCMakeLibs(install_lib):
-    """
-    Get the libraries from the parent distribution, use those as the outfiles
-
-    Skip building anything; everything is already built, forward libraries to
-    the installation step
-    """
-
-    def run(self):
-        """
-        Copy libraries from the bin directory and place them as appropriate
-        """
-
-        self.announce("Moving library files", level=3)
-
-        # We have already built the libraries in the previous build_ext step
-
-        self.skip_build = True
-
-        bin_dir = self.distribution.bin_dir
-
-        # Depending on the files that are generated from your cmake
-        # build chain, you may need to change the below code, such that
-        # your files are moved to the appropriate location when the installation
-        # is run
-
-        libs = [os.path.join(bin_dir, _lib) for _lib in 
-                os.listdir(bin_dir) if 
-                os.path.isfile(os.path.join(bin_dir, _lib)) and 
-                os.path.splitext(_lib)[1] in [".dll", ".so"]
-                and not (_lib.startswith("python") or _lib.startswith(PACKAGE_NAME))]
-
-        for lib in libs:
-
-            shutil.move(lib, os.path.join(self.build_dir,
-                                          os.path.basename(lib)))
-
-        # Mark the libs for installation, adding them to 
-        # distribution.data_files seems to ensure that setuptools' record 
-        # writer appends them to installed-files.txt in the package's egg-info
-        #
-        # Also tried adding the libraries to the distribution.libraries list, 
-        # but that never seemed to add them to the installed-files.txt in the 
-        # egg-info, and the online recommendation seems to be adding libraries 
-        # into eager_resources in the call to setup(), which I think puts them 
-        # in data_files anyways. 
-        # 
-        # What is the best way?
-
-        # These are the additional installation files that should be
-        # included in the package, but are resultant of the cmake build
-        # step; depending on the files that are generated from your cmake
-        # build chain, you may need to modify the below code
-
-        self.distribution.data_files = [os.path.join(self.install_dir, 
-                                                     os.path.basename(lib))
-                                        for lib in libs]
-
-        # Must be forced to run after adding the libs to data_files
-
-        self.distribution.run_command("install_data")
-
+        for ext in self.extensions:
+            self.build_cmake(ext)
         super().run()
 
-class BuildCMakeExt(build_ext):
-    """
-    Builds using cmake instead of the python setuptools implicit build
-    """
+    def build_cmake(self, ext):
+        cwd = pathlib.Path().absolute()
 
-    def run(self):
-        """
-        Perform build_cmake before doing the 'normal' stuff
-        """
+        # these dirs will be created in build_py, so if you don't have
+        # any python sources to bundle, the dirs will be missing
+        build_temp = pathlib.Path(self.build_temp)
+        build_temp.mkdir(parents=True, exist_ok=True)
+        # Our extension is a module path and not a directory
+        extlib = pathlib.Path(self.get_ext_fullpath(ext.name))
+        extdir = extlib.parent
+        extdir.mkdir(parents=True, exist_ok=True)
 
-        for extension in self.extensions:
-
-            if extension.name == PACKAGE_NAME:
-
-                self.build_cmake(extension)
-
-        super().run()
-
-    def build_cmake(self, extension: Extension):
-        """
-        The steps required to build the extension
-        """
-
-        self.announce("Preparing the build environment", level=3)
-
-        build_dir = pathlib.Path(self.build_temp)
-
-        extension_path = pathlib.Path(self.get_ext_fullpath(extension.name))
-
-        os.makedirs(build_dir, exist_ok=True)
-        os.makedirs(extension_path.parent.absolute(), exist_ok=True)
-
-        # Now that the necessary directories are created, build
-
-        self.announce("Configuring cmake project", level=3)
-
-        # Change your cmake arguments below as necessary
-        # Below is just an example set of arguments for building Blender as a Python module
-        
         config = 'Debug' if self.debug else 'Release'
         cmake_args = [
             '-DRUN_DOXYGEN=OFF',
             f'-DPYTHON_EXECUTABLE={sys.executable}',
-            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + str(extension_path.parent.absolute()),
+            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + str(extdir.absolute()),
             '-DCMAKE_BUILD_TYPE=' + config
         ]
 
-        self.spawn(['cmake', '-H'+SOURCE_DIR, '-B'+self.build_temp] + cmake_args)
+        build_args = [
+            '--config', config,
+            '--', '-j16'
+        ]
 
-        self.announce("Building binaries", level=3)
+        os.chdir(str(build_temp))
+        self.spawn(['cmake', str(cwd)] + cmake_args)
+        if not self.dry_run:
+            self.spawn(['cmake', '--build', '.'] + build_args)
+        # Troubleshooting: if fail on line above then delete all possible 
+        # temporary CMake files including "CMakeCache.txt" in top level dir.
+        os.chdir(str(cwd))
 
-        self.spawn(["cmake", "--build", self.build_temp, "--target", "INSTALL",
-                    "--config", "Release"])
-
-        # We already put all the libraries in the right output directory
-        # (parent of the extension itself).
-
-        # After build_ext is run, the following commands will run:
-        # 
-        # install_lib
-        # install_scripts
-        # 
-        # These commands are subclassed above to avoid pitfalls that
-        # setuptools tries to impose when installing these, as it usually
-        # wants to build those libs and scripts as well or move them to a
-        # different place. See comments above for additional information
 
 setup(
-    name=PACKAGE_NAME,
+    name='bdsg',
     version='0.1',
-    packages=[PACKAGE_NAME],
-    ext_modules=[CMakeExtension(PACKAGE_NAME)],
+    packages=['bdsg'],
+    ext_modules=[CMakeExtension('bdsg')],
     cmdclass={
-        'build_ext': BuildCMakeExt,
-        'install_data': InstallCMakeLibsData,
-        'install_lib': InstallCMakeLibs,
+        'build_ext': build_ext,
     }
 )
