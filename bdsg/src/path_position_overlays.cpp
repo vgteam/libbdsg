3e762cf26feec58a0adce25e1c9b8c485a40bc74
Adam Novak <anovak@soe.ucsc.edu>
2021-06-10 14:55:32 -0700

    Fix Clang warning about casting and virtual inheritance

--- bdsg/src/path_position_overlays.cpp
+++ bdsg/src/path_position_overlays.cpp
@@ -207,7 +207,7 @@ namespace bdsg {
         });
     }
     
-    MutablePositionOverlay::MutablePositionOverlay(MutablePathDeletableHandleGraph* graph) : PositionOverlay(graph) {
+    MutablePositionOverlay::MutablePositionOverlay(MutablePathDeletableHandleGraph* graph) : PositionOverlay(graph), mutable_graph(graph) {
         
     }
     
@@ -428,6 +428,6 @@ namespace bdsg {
     }
     
     MutablePathDeletableHandleGraph* MutablePositionOverlay::get_graph() {
-        return static_cast<MutablePathDeletableHandleGraph*>(graph);
+        return mutable_graph;
     }
 }
