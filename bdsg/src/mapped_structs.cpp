adc0a704d3b958a83ad72063dcc0aa2d1551e231
Xian Chang <xhchang@ucsc.edu>
2021-09-24 11:07:14 -0700

    Print whether a link is mapped in dump_links

--- bdsg/src/mapped_structs.cpp
+++ bdsg/src/mapped_structs.cpp
@@ -583,7 +583,12 @@ void Manager::dump(chainid_t chain) {
 void Manager::dump_links(ostream& out) {
     out << "All chain links: " << std::endl;
     for (auto& link : address_space_index) {
-        out << "\t" << (void*)link.first << "-" << (void*)(link.first + link.second.length) << " in chain " << link.second.first << " with next link at " << link.second.next << std::endl; 
+        out << "\t" << (void*)link.first 
+            << "-" << (void*)(link.first + link.second.length) 
+            << " in chain " << link.second.first 
+            << " with next link at " << link.second.next 
+            << " is " << (link.second.mapping == nullptr ? " not " : "" ) << "mapped" 
+            << std::endl; 
     }
 }
     
