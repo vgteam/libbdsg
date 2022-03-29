84b2a223ff58090198e59d74a1bb0766e7411b0f
Adam Novak <anovak@soe.ucsc.edu>
2022-03-29 17:16:07 -0400

    Use more general path search API

--- bdsg/cmake_bindings/bdsg/overlays/vectorizable_overlays_1.cpp
+++ bdsg/cmake_bindings/bdsg/overlays/vectorizable_overlays_1.cpp
@@ -9,6 +9,8 @@
 #include <memory>
 #include <sstream> // __str__
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include <utility>
 #include <vector>
 
@@ -775,18 +777,18 @@ struct PyCallBack_bdsg_PathPositionVectorizableOverlay : public bdsg::PathPositi
 		}
 		return PathMetadata::get_subrange(a0);
 	}
-	bool for_each_path_of_sense_impl(const enum handlegraph::PathMetadata::Sense & a0, const class std::function<bool (const struct handlegraph::path_handle_t &)> & a1) const override { 
+	bool for_each_path_matching_impl(const class std::unordered_set<enum handlegraph::PathMetadata::Sense, struct std::hash<enum handlegraph::PathMetadata::Sense>, struct std::equal_to<enum handlegraph::PathMetadata::Sense>, class std::allocator<enum handlegraph::PathMetadata::Sense> > * a0, const class std::unordered_set<std::string, struct std::hash<std::string>, struct std::equal_to<std::string >, class std::allocator<std::string > > * a1, const class std::unordered_set<std::string, struct std::hash<std::string>, struct std::equal_to<std::string >, class std::allocator<std::string > > * a2, const class std::function<bool (const struct handlegraph::path_handle_t &)> & a3) const override { 
 		pybind11::gil_scoped_acquire gil;
-		pybind11::function overload = pybind11::get_overload(static_cast<const bdsg::PathPositionVectorizableOverlay *>(this), "for_each_path_of_sense_impl");
+		pybind11::function overload = pybind11::get_overload(static_cast<const bdsg::PathPositionVectorizableOverlay *>(this), "for_each_path_matching_impl");
 		if (overload) {
-			auto o = overload.operator()<pybind11::return_value_policy::reference>(a0, a1);
+			auto o = overload.operator()<pybind11::return_value_policy::reference>(a0, a1, a2, a3);
 			if (pybind11::detail::cast_is_temporary_value_reference<bool>::value) {
 				static pybind11::detail::overload_caster_t<bool> caster;
 				return pybind11::detail::cast_ref<bool>(std::move(o), caster);
 			}
 			else return pybind11::detail::cast_safe<bool>(std::move(o));
 		}
-		return PathMetadata::for_each_path_of_sense_impl(a0, a1);
+		return PathMetadata::for_each_path_matching_impl(a0, a1, a2, a3);
 	}
 	bool for_each_step_of_sense_impl(const struct handlegraph::handle_t & a0, const enum handlegraph::PathMetadata::Sense & a1, const class std::function<bool (const struct handlegraph::step_handle_t &)> & a2) const override { 
 		pybind11::gil_scoped_acquire gil;
--- bdsg/cmake_bindings/handlegraph/path_metadata.cpp
+++ bdsg/cmake_bindings/handlegraph/path_metadata.cpp
@@ -7,6 +7,8 @@
 #include <memory>
 #include <sstream> // __str__
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include <utility>
 #include <vector>
 
@@ -27,7 +29,7 @@
 	PYBIND11_MAKE_OPAQUE(std::shared_ptr<void>);
 #endif
 
-// handlegraph::PathMetadata file:handlegraph/path_metadata.hpp line:55
+// handlegraph::PathMetadata file:handlegraph/path_metadata.hpp line:56
 struct PyCallBack_handlegraph_PathMetadata : public handlegraph::PathMetadata {
 	using handlegraph::PathMetadata::PathMetadata;
 
@@ -110,18 +112,18 @@ struct PyCallBack_handlegraph_PathMetadata : public handlegraph::PathMetadata {
 		}
 		return PathMetadata::get_subrange(a0);
 	}
-	bool for_each_path_of_sense_impl(const enum handlegraph::PathMetadata::Sense & a0, const class std::function<bool (const struct handlegraph::path_handle_t &)> & a1) const override { 
+	bool for_each_path_matching_impl(const class std::unordered_set<enum handlegraph::PathMetadata::Sense, struct std::hash<enum handlegraph::PathMetadata::Sense>, struct std::equal_to<enum handlegraph::PathMetadata::Sense>, class std::allocator<enum handlegraph::PathMetadata::Sense> > * a0, const class std::unordered_set<std::string, struct std::hash<std::string>, struct std::equal_to<std::string >, class std::allocator<std::string > > * a1, const class std::unordered_set<std::string, struct std::hash<std::string>, struct std::equal_to<std::string >, class std::allocator<std::string > > * a2, const class std::function<bool (const struct handlegraph::path_handle_t &)> & a3) const override { 
 		pybind11::gil_scoped_acquire gil;
-		pybind11::function overload = pybind11::get_overload(static_cast<const handlegraph::PathMetadata *>(this), "for_each_path_of_sense_impl");
+		pybind11::function overload = pybind11::get_overload(static_cast<const handlegraph::PathMetadata *>(this), "for_each_path_matching_impl");
 		if (overload) {
-			auto o = overload.operator()<pybind11::return_value_policy::reference>(a0, a1);
+			auto o = overload.operator()<pybind11::return_value_policy::reference>(a0, a1, a2, a3);
 			if (pybind11::detail::cast_is_temporary_value_reference<bool>::value) {
 				static pybind11::detail::overload_caster_t<bool> caster;
 				return pybind11::detail::cast_ref<bool>(std::move(o), caster);
 			}
 			else return pybind11::detail::cast_safe<bool>(std::move(o));
 		}
-		return PathMetadata::for_each_path_of_sense_impl(a0, a1);
+		return PathMetadata::for_each_path_matching_impl(a0, a1, a2, a3);
 	}
 	bool for_each_step_of_sense_impl(const struct handlegraph::handle_t & a0, const enum handlegraph::PathMetadata::Sense & a1, const class std::function<bool (const struct handlegraph::step_handle_t &)> & a2) const override { 
 		pybind11::gil_scoped_acquire gil;
@@ -780,18 +782,18 @@ struct PyCallBack_handlegraph_PathHandleGraph : public handlegraph::PathHandleGr
 		}
 		return PathMetadata::get_subrange(a0);
 	}
-	bool for_each_path_of_sense_impl(const enum handlegraph::PathMetadata::Sense & a0, const class std::function<bool (const struct handlegraph::path_handle_t &)> & a1) const override { 
+	bool for_each_path_matching_impl(const class std::unordered_set<enum handlegraph::PathMetadata::Sense, struct std::hash<enum handlegraph::PathMetadata::Sense>, struct std::equal_to<enum handlegraph::PathMetadata::Sense>, class std::allocator<enum handlegraph::PathMetadata::Sense> > * a0, const class std::unordered_set<std::string, struct std::hash<std::string>, struct std::equal_to<std::string >, class std::allocator<std::string > > * a1, const class std::unordered_set<std::string, struct std::hash<std::string>, struct std::equal_to<std::string >, class std::allocator<std::string > > * a2, const class std::function<bool (const struct handlegraph::path_handle_t &)> & a3) const override { 
 		pybind11::gil_scoped_acquire gil;
-		pybind11::function overload = pybind11::get_overload(static_cast<const handlegraph::PathHandleGraph *>(this), "for_each_path_of_sense_impl");
+		pybind11::function overload = pybind11::get_overload(static_cast<const handlegraph::PathHandleGraph *>(this), "for_each_path_matching_impl");
 		if (overload) {
-			auto o = overload.operator()<pybind11::return_value_policy::reference>(a0, a1);
+			auto o = overload.operator()<pybind11::return_value_policy::reference>(a0, a1, a2, a3);
 			if (pybind11::detail::cast_is_temporary_value_reference<bool>::value) {
 				static pybind11::detail::overload_caster_t<bool> caster;
 				return pybind11::detail::cast_ref<bool>(std::move(o), caster);
 			}
 			else return pybind11::detail::cast_safe<bool>(std::move(o));
 		}
-		return PathMetadata::for_each_path_of_sense_impl(a0, a1);
+		return PathMetadata::for_each_path_matching_impl(a0, a1, a2, a3);
 	}
 	bool for_each_step_of_sense_impl(const struct handlegraph::handle_t & a0, const enum handlegraph::PathMetadata::Sense & a1, const class std::function<bool (const struct handlegraph::step_handle_t &)> & a2) const override { 
 		pybind11::gil_scoped_acquire gil;
@@ -810,7 +812,7 @@ struct PyCallBack_handlegraph_PathHandleGraph : public handlegraph::PathHandleGr
 
 void bind_handlegraph_path_metadata(std::function< pybind11::module &(std::string const &namespace_) > &M)
 {
-	{ // handlegraph::PathMetadata file:handlegraph/path_metadata.hpp line:55
+	{ // handlegraph::PathMetadata file:handlegraph/path_metadata.hpp line:56
 		pybind11::class_<handlegraph::PathMetadata, std::shared_ptr<handlegraph::PathMetadata>, PyCallBack_handlegraph_PathMetadata> cl(M("handlegraph"), "PathMetadata", "This is the interface for embedded path and haplotype thread metadata.\n\n Comes with a default implementation of this interface, based on\n a get_path_name() and special path name formatting.\n\n Our model is that paths come in different \"senses\":\n\n - SENSE_GENERIC: a generic named path. Has a \"locus\" name.\n\n - SENSE_REFERENCE: a part of a reference assembly. Has a \"sample\" name, a\n   \"locus\" name, and a haplotype number.\n\n - SENSE_HAPLOTYPE: a haplotype from a particular individual. Has a \"sample\"\n   name, a \"locus\" name, a haplotype number, and a phase block identifier.\n\n Paths of all sneses can represent subpaths, with bounds.\n\n Depending on sense, a path might have:\n\n - Sample: sample or assembly name.\n\n - Locus: contig, scaffold, or gene name path either represents in its\n   assembly or is an allele of in its sample.\n\n - Haplotype number: number identifying which haplotype of a locus is being\n   represented. GFA uses a convention where the presence of a haplotype 0\n   implies that only one haplotype is present.\n\n - Phase block identifier: Distinguishes fragments of a haplotype that are\n   phased but not necessarily part of a single self-consistent scaffold (often\n   due to self-contradictory VCF information). Must be unique within a sample,\n   locus, and haplotype. May be a number or a start coordinate.\n\n - Bounds, for when a path as stored gives only a sub-range of a conceptually\n   longer path. Multiple items can be stored with identical metadata in the\n   other fields if their bounds are non-overlapping.\n TODO: Interaction with phase block in GBWT???");
 		cl.def(pybind11::init<PyCallBack_handlegraph_PathMetadata const &>());
 		cl.def( pybind11::init( [](){ return new PyCallBack_handlegraph_PathMetadata(); } ) );
