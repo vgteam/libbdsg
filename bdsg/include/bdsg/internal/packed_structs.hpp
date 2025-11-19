13ea17d23e7c3ba68138351a98cf8b54d97983e0
Adam Novak <anovak@soe.ucsc.edu>
2025-11-19 18:28:08 -0500

    Fluff up iterator into a random access iterator with comparison

--- bdsg/include/bdsg/internal/packed_structs.hpp
+++ bdsg/include/bdsg/internal/packed_structs.hpp
@@ -40,7 +40,7 @@ template<typename VectorLike>
 class IndexingIterator {
 public:
     // Iterator traits for standard library compatibility
-    using iterator_category = std::forward_iterator_tag;
+    using iterator_category = std::random_access_iterator_tag;
     using value_type = uint64_t;
     using difference_type = typename std::make_signed<size_t>::type;
     using pointer = void;
@@ -66,6 +66,55 @@ public:
     /// Inequality comparison
     bool operator!=(const IndexingIterator& other) const;
 
+    // Bidirectional methods
+
+    /// Pre-decrement operator
+    IndexingIterator& operator--();
+
+    /// Post-decrement operator
+    IndexingIterator operator--(int);
+
+    // Random-access methods
+    
+    /// Addition of offset in place
+    IndexingIterator& operator+=(difference_type offset);
+
+    /// Addition of offset
+    IndexingIterator operator+(difference_type offset) const;
+
+    /// Subtraction of offset in place
+    IndexingIterator& operator-=(difference_type offset);
+
+    /// Subtraction of offset
+    IndexingIterator operator-(difference_type offset) const;
+
+    /// Subtraction of two iterators
+    difference_type operator-(const IndexingIterator& other) const;
+
+    /// Indexing into iterator. Even though we type this as reference, remember
+    /// that we don't actually implement writing to our "references" and just
+    /// use the value type.
+    /// Result is undefined if itrators are to different collecitons.
+    reference operator[](difference_type offset) const;
+
+    // Comaprable iterator methods (TODO: Is there an STL concept name for this?)
+    
+    /// Determine if this iterator is strictly before another.
+    /// Result is undefined if iterators are to different collecitons.
+    bool operator<(const IndexingIterator& other) const;
+
+    /// Determine if this iterator is before or at another.
+    /// Result is undefined if iterators are to different collecitons.
+    bool operator<=(const IndexingIterator& other) const;
+
+    /// Determine if this iterator is strictly after another.
+    /// Result is undefined if iterators are to different collecitons.
+    bool operator>(const IndexingIterator& other) const;
+
+    /// Determine if this iterator is at or after another.
+    /// Result is undefined if itrators are to different collecitons.
+    bool operator>=(const IndexingIterator& other) const;
+    
 private:
     // Private constructor - only associated class can create iterators
     IndexingIterator(const VectorLike* vec, size_t idx);
@@ -685,6 +734,75 @@ template<typename VectorLike>
 bool IndexingIterator<VectorLike>::operator!=(const IndexingIterator& other) const {
     return !(*this == other);
 }
+
+template<typename VectorLike>
+IndexingIterator<VectorLike>& IndexingIterator<VectorLike>::operator--() {
+    --index;
+    return *this;
+}
+
+template<typename VectorLike>
+IndexingIterator<VectorLike> IndexingIterator<VectorLike>::operator--(int) {
+    IndexingIterator tmp = *this;
+    --index;
+    return tmp;
+}
+
+template<typename VectorLike>
+IndexingIterator<VectorLike>& IndexingIterator<VectorLike>::operator+=(difference_type offset) {
+    index += offset;
+    return *this;
+}
+
+template<typename VectorLike>
+IndexingIterator<VectorLike> IndexingIterator<VectorLike>::operator+(difference_type offset) const {
+    return IndexingIterator(vec_ptr, index + offset);
+}
+
+template<typename VectorLike>
+IndexingIterator<VectorLike>& IndexingIterator<VectorLike>::operator-=(difference_type offset) {
+    index -= offset;
+    return *this;
+}
+
+template<typename VectorLike>
+IndexingIterator<VectorLike> IndexingIterator<VectorLike>::operator-(difference_type offset) const {
+    return IndexingIterator(vec_ptr, index - offset);
+}
+
+template<typename VectorLike>
+typename IndexingIterator<VectorLike>::difference_type IndexingIterator<VectorLike>::operator-(const IndexingIterator& other) const {
+    // TODO: I don't know a way to subtract two unsigned values and get the
+    // signed difference in a single operation as long as that difference
+    // itself fits the signed type. So we cast and hope.
+    return (difference_type) index - (difference_type) other.index;
+}
+
+template<typename VectorLike>
+typename IndexingIterator<VectorLike>::reference IndexingIterator<VectorLike>::operator[](difference_type offset) const {
+    return *(*this + offset);
+}
+
+template<typename VectorLike>
+bool IndexingIterator<VectorLike>::operator<(const IndexingIterator& other) const {
+    return index < other.index;
+}
+
+template<typename VectorLike>
+bool IndexingIterator<VectorLike>::operator<=(const IndexingIterator& other) const {
+    return index <= other.index;
+}
+
+template<typename VectorLike>
+bool IndexingIterator<VectorLike>::operator>(const IndexingIterator& other) const {
+    return index > other.index;
+}
+
+template<typename VectorLike>
+bool IndexingIterator<VectorLike>::operator>=(const IndexingIterator& other) const {
+    return index >= other.index;
+}
+
 
 /////////////////////
 /// PackedVector
